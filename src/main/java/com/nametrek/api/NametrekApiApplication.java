c7d68e7451fa26867e2f6701b17c57dd7e6fad7b
ideftbuild <a.akanSWE@gamil.com>
2024-12-19 21:55:20 +0100

    feat: Add spring boot application class

---
+++ src/main/java/com/nametrek/api/NametrekApiApplication.java
@@ -0,0 +1,14 @@
+package com.nametrek.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class NametrekApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NametrekApiApplication.class, args);
+	}
+
+}
