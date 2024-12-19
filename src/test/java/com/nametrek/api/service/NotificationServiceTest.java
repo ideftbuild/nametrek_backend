4e148ce724a85129c8a1d33a922b6a93b94a85ac
ideftbuild <a.akanSWE@gamil.com>
2024-12-19 22:08:58 +0100

    feat: Add test class

---
+++ src/test/java/com/nametrek/api/service/NotificationServiceTest.java
@@ -0,0 +1,33 @@
+package com.nametrek.api.service;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+
+@ExtendWith(MockitoExtension.class)
+public class NotificationServiceTest {
+
+    @Mock
+    private SimpMessagingTemplate messagingTemplate;
+
+    @InjectMocks
+    private NotificationService notificationService;
+
+    /**
+     * Verify that method sends message to a topic
+     */
+    @Test
+    public void testSendMessageToTopic() {
+        doNothing().when(messagingTemplate).convertAndSend("/game/room/1", "Hello");
+
+        notificationService.sendMessageToTopic("/room/1", "Hello");
+
+        verify(messagingTemplate).convertAndSend("/game/room/1", "Hello");
+    }
+}
