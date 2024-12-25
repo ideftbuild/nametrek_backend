24187a9e66a4db23056852b8dcd0736eb851897f
ideftbuild <a.akanSWE@gamil.com>
2024-12-25 18:04:04 +0100

    refactor: comment out unused field from class

--- src/main/java/com/nametrek/api/dto/PlayerDto.java
+++ src/main/java/com/nametrek/api/dto/PlayerDto.java
@@ -1,14 +1,16 @@
 package com.nametrek.api.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@AllArgsConstructor
 public class PlayerDto {
     // private String id;
     private String username;
     private Integer score;
-    private String roomId;
+    // private String roomId/* ; */
 }
 
